e279627ab88d28ed3a5982c77013e07d0f93a247
Gayan Chinthaka <chinthaka@blackswan-technologies.com>
2022-06-03 21:42:46 +0530

    initial commit

---
+++ src/main/java/com/chiku/dist/dmsrest/DmsrestApplication.java
@@ -0,0 +1,13 @@
+package com.chiku.dist.dmsrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmsrestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DmsrestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chiku/dist/dmsrest/DmsrestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chiku.dist.dmsrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmsrestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
