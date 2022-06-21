3398b91049c78f71aa5bc00ad28a0a7289379c57
Gayan Chinthaka <chinthaka@blackswan-technologies.com>
2022-06-21 14:48:32 +0530

    poc_v6

--- dmsdeploy/builder.sh
+++ dmsdeploy/builder.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 NOW=$(date +"%m%d%Y%H%M%S")
 export CURRENT_DIR=`pwd`
-export D_TAG_VERSION=poc_${NOW}
+export D_TAG_VERSION=poc_v6
 echo ${CURRENT_DIR}
-cd ${CURRENT_DIR}/../dmsrest && sh ./build.sh
-cd ${CURRENT_DIR}/../dmswebui && sh ./build.sh
+cd ${CURRENT_DIR}/../ && sh ./build.sh
+cd ${CURRENT_DIR}/../../dmswebui && sh ./build.sh
 docker images
 docker tag dmsserv:${D_TAG_VERSION} chiku153/dmsrest:${D_TAG_VERSION}
-docker push chiku153/dmsrest:${D_TAG_VERSION}
+#docker push chiku153/dmsrest:${D_TAG_VERSION}
 docker tag dmswebui:${D_TAG_VERSION} chiku153/dmsui:${D_TAG_VERSION}
-docker push chiku153/dmsui:${D_TAG_VERSION}
+#docker push chiku153/dmsui:${D_TAG_VERSION}
